8c571c228fa021db39acdabba6bc133aa30ec6ea
Dominik Schildorfer <dominik.schildorfer@gmail.com>
2013-10-15 19:00:13 +0200

    RegionLoader added

---
+++ src/main/java/com/mycompany/deliverysystem/RegionLoader.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.deliverysystem;
+
+/**
+ *
+ * @author dominik,rafael
+ */
+public class RegionLoader {
+    
+    public void loadRegions(){
+    
+    }
+}
